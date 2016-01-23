3351368b6623fd47d560067b3cd0b7a78ac079f2
torresanfederico <ftfedericotorresan@gmail.com>
2016-01-23 09:37:10 +0100

    Grafica

--- SpesaGrafica/src/prodotto/Grafica.java
+++ SpesaGrafica/src/prodotto/Grafica.java
@@ -7,10 +7,16 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.custom.StyledText;
 
 public class Grafica {
 
 	protected Shell shlSwtApplication;
+	public boolean tessera=false;
+	public listaSpesa ls = new listaSpesa(tessera);
 	private Text codice;
 	private Text prezzo;
 	private Text descrizione;
@@ -72,17 +78,73 @@ public class Grafica {
 		descrizione = new Text(shlSwtApplication, SWT.BORDER);
 		descrizione.setBounds(100, 103, 177, 58);
 		
-		Label lblAlimentare = new Label(shlSwtApplication, SWT.NONE);
-		lblAlimentare.setBounds(10, 207, 69, 15);
-		lblAlimentare.setText("Alimentare");
+		Button btnSalva = new Button(shlSwtApplication, SWT.NONE);
+		btnSalva.setBounds(387, 477, 75, 25);
+		btnSalva.setText("Salva");
 		
-		Button btnS = new Button(shlSwtApplication, SWT.CHECK);
-		btnS.setBounds(100, 207, 93, 16);
-		btnS.setText("S\u00EC");
+		Button btnCarica = new Button(shlSwtApplication, SWT.NONE);
+		btnCarica.setBounds(468, 477, 75, 25);
+		btnCarica.setText("Carica");
+		
+		Button btnTotale = new Button(shlSwtApplication, SWT.NONE);
+		btnTotale.setBounds(549, 477, 75, 25);
+		btnTotale.setText("Totale");
+		
+		Label lblTessera = new Label(shlSwtApplication, SWT.NONE);
+		lblTessera.setBounds(10, 186, 55, 15);
+		lblTessera.setText("Tessera");
+		
+		Button btnSi = new Button(shlSwtApplication, SWT.CHECK);
+		btnSi.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+			}
+		});
+		btnSi.setBounds(136, 184, 30, 16);
+		btnSi.setText("Si");
 		
 		Button btnNo = new Button(shlSwtApplication, SWT.CHECK);
-		btnNo.setBounds(145, 207, 93, 16);
+		btnNo.setBounds(179, 185, 42, 16);
 		btnNo.setText("No");
+		
+		Button btnAggiungi = new Button(shlSwtApplication, SWT.NONE);
+		btnAggiungi.setBounds(306, 477, 75, 25);
+		btnAggiungi.setText("Aggiungi");
+		
+		List list = new List(shlSwtApplication, SWT.BORDER);
+		list.setBounds(361, 207, 214, 213);
+		
+		Label lblListaSpesa = new Label(shlSwtApplication, SWT.NONE);
+		lblListaSpesa.setBounds(361, 186, 69, 15);
+		lblListaSpesa.setText("Lista Spesa");
+		
+		Button btnAlimentare = new Button(shlSwtApplication, SWT.RADIO);
+		btnAlimentare.setText("Alimentare");
+		btnAlimentare.setBounds(10, 225, 90, 16);
+		
+		Button btnNonAlimentare = new Button(shlSwtApplication, SWT.RADIO);
+		btnNonAlimentare.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnNonAlimentare.setBounds(10, 280, 111, 16);
+		btnNonAlimentare.setText("Non alimentare");
+		
+		Label lblMateriale = new Label(shlSwtApplication, SWT.NONE);
+		lblMateriale.setBounds(38, 302, 55, 15);
+		lblMateriale.setText("Materiale");
+		
+		Text materiale = new Text(shlSwtApplication, SWT.BORDER);
+		materiale.setBounds(136, 299, 111, 21);
+		
+		Label lblDataScadenza = new Label(shlSwtApplication, SWT.NONE);
+		lblDataScadenza.setBounds(38, 247, 83, 15);
+		lblDataScadenza.setText("Data scadenza");
+		
+		DateTime dateTime = new DateTime(shlSwtApplication, SWT.BORDER);
+		dateTime.setBounds(136, 247, 80, 24);
 
 	}
 }
